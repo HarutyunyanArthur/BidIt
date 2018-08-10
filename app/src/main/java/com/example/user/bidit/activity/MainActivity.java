580ac5ef256d580f7b5a8950079df429737d0748
Artur_H <harutyunyan.artur.91@gmail.com>
2018-08-10 21:29:37 +0400

    08.10

--- app/src/main/java/com/example/user/bidit/activity/MainActivity.java
+++ app/src/main/java/com/example/user/bidit/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.bidit.activity;
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,92 +14,108 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.example.user.bidit.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawer;
+    private Toolbar mToolbar;
+    private FrameLayout mContainer;
+    private boolean mCreated = false;
+    private CategoryFragment mCategoryFragment = new CategoryFragment();
+    private android.app.FragmentManager mFragmentManager;
+    private android.app.FragmentTransaction mFragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        initViews();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setNavigationBar();
+        updateScreen(mCategoryFragment);
+        updateNavBar();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        switch (item.getItemId()) {
+            case R.id.nav_account:
+                break;
+            case R.id.nav_balance:
+                break;
+            case R.id.nav_history:
+                break;
+            case R.id.nav_favorite:
+                break;
+            case R.id.nav_my_items:
+                break;
+            case R.id.nav_log_out:
+                mCreated = true;
+                break;
+            case R.id.nav_create_account:
+                mCreated = false;
+                Intent intent = new Intent(MainActivity.this, EmailPasswordActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_help:
+                break;
+            case R.id.nav_about_us:
+                break;
 
         }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        updateNavBar();
+        mDrawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void initViews() {
+        mToolbar = findViewById(R.id.toolbar);
+        mDrawer = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+        mContainer = findViewById(R.id.container_main);
+        mFragmentManager = getFragmentManager();
+    }
+
+    public void setNavigationBar() {
+        setSupportActionBar(mToolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    public void updateScreen(Fragment fragment) {
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.add(R.id.container_main, fragment);
+        mFragmentTransaction.commit();
+
+    }
+
+    public void updateNavBar() {
+        if (mCreated) {
+            mNavigationView.getMenu().setGroupVisible(R.id.menu_group_if_created, false);
+            mNavigationView.getMenu().findItem(R.id.nav_create_account).setVisible(true);
+        } else {
+            mNavigationView.getMenu().setGroupVisible(R.id.menu_group_if_created, true);
+            mNavigationView.getMenu().findItem(R.id.nav_create_account).setVisible(false);
+        }
     }
 }
